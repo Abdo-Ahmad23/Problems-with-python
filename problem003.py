daf99a9bd641a4507345daf742c1ea341b034606
Abdurahman Abdelhameed <thisforwork23@gmail.com>
2024-08-06 12:57:06 +0300

    Add filename.py

---
+++ problem003.py
@@ -0,0 +1,13 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+s = input()
+
+if(s=='00'):
+    print('Nothing Lol')
+else:
+    tmp='#'*int(s[0])+'*'*int(s[1])
+    print(tmp)
