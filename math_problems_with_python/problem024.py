a17690211efb134f8423dc3a6489922cd4434f10
Abdo-Ahmad <thisforwork23@gmail.com>
2024-08-19 20:06:49 +0300

    problem024

--- math_problems_with_python/problem024.py
+++ math_problems_with_python/problem024.py
@@ -5,13 +5,12 @@ import math
 # the standard input according to the problem statement.
 
 s = input()
-
-s=sorted(s)
-f=int(s[0])
-l=int(s[-2])
-cnt=0
-for i in range(f,l):
-    if s[cnt]!=str(i):
-        print(i)
-        break
-    cnt+=1
+tmp=''
+for i in s:
+    if i.isupper():
+        tmp+=str(ord(i)-ord('A')+1)
+    elif i.islower():
+        tmp+=str(ord(i)-ord('a')+1)
+    else:
+        tmp+=i
+print(tmp)
