9fb926ad6e32031f5188bd085efd39320445fdd9
Abdo-Ahmad <thisforwork23@gmail.com>
2024-08-21 15:05:24 +0300

    problem028

--- math_problems_with_python/problem028.py
+++ math_problems_with_python/problem028.py
@@ -4,6 +4,28 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-a = input()
+s = input()
+m = input()
 
-print(len(a)//2)
+dots=len(s)-1
+
+if m=='|':
+    print(s[::-1])
+elif m=='\\':
+    s=s[::-1]
+    for i in range(len(s)):
+        tmp=''
+        for j in range(dots):
+            tmp+='.'
+        tmp+=s[i]
+        print(tmp)
+        dots-=1
+else:
+    s=s[::-1]
+    for i in range(len(s)):
+        tmp=''
+        for j in range(i):
+            tmp+='.'
+        tmp+=s[i]
+        print(tmp)
+        # dots-=1
